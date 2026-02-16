52e901a8d7b8e2991668d7e6011b1db244eb60ff
Devensingh09 <devensingh.chauhan09@gmail.com>
2026-02-16 09:48:52 +0530

    Added

---
+++ index.js
@@ -0,0 +1,21 @@
+// var express = require("express")
+// var app = express();
+// app.get('/',function(req,resp){
+//     resp.send("Welcom to REST API")
+// })
+// app.listen(9000,()=> console.log("API Started Listening"))
+var express = require("express")
+var app = express();
+app.get('/',function(req,resp){
+    resp.send("Hello from API")
+});
+app.get('/time',function(req,resp){
+    var time= new Date().toLocaleTimeString();
+    resp.send(`Time is:${time}`);
+});
+app.get('/date',function(req, resp){
+    var date = new Date().toLocaleDateString();
+    resp.send(`Date is: ${date}`);
+})
+
+app.listen(9000,()=> console.log("API Started Listening"))
